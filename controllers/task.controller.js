7a0333f199602b9abccafe541654569b8736bba1
Deepthi Pauly <deepthipaul.c@gmail.com>
2023-06-23 11:16:42 +0530

    created taskcontroller

---
+++ controllers/task.controller.js
@@ -0,0 +1,80 @@
+
+
+
+const addTaskController = async (req, res) => {
+  console.log("START: addTaskController");
+  try {
+    req.body.userId = req.userId;
+    const addNewTask = await addNewTodo(req.body);
+    return res.status(201).json({
+      data: addNewTask,
+      message: "Todo is added",
+    });
+  } catch (e) {
+    return res.status(400).json({ error: e.message || "something went wrong" });
+  }
+};
+
+const editTaskController = async (req, res) => {
+  console.log("START: editTaskController");
+  try {
+    req.body.todoId = req.params.todoId;
+    req.body.userId = req.userId;
+    const editedTask = await editTodo(req.body);
+    return res.status(201).json({
+      data: editedTask,
+      message: "Todo updated",
+    });
+  } catch (e) {
+    return res.status(400).json({ error: e.message || "something went wrong" });
+  }
+};
+
+const deleteTaskController = async (req, res) => {
+  console.log("START: deleteTaskController");
+  try {
+    await deleteTodo(req.params.todoId, req.userId);
+    return res.status(201).json({
+      data: [],
+      message: "Todo deleted",
+    });
+  } catch (e) {
+    return res.status(400).json({ error: e.message || "something went wrong" });
+  }
+};
+
+const allTaskController = async (req, res) => {
+  console.log("START: allTaskController");
+
+  try {
+    const fetchAllTask = await viewAllTask(req.userId);
+    return res.status(201).json({
+      data: fetchAllTask,
+      message: "All Todo fetched successfully",
+    });
+  } catch (e) {
+    return res.status(400).json({ error: e.message || "something went wrong" });
+  }
+};
+
+const viewSingleTaskController = async (req, res) => {
+  console.log("START: viewSingleTaskController");
+
+  try {
+    const viewSingleTask = await viewTask(req.params.todoId, req.userId);
+    return res.status(201).json({
+      data: viewSingleTask,
+      message: "Single Todo fetched",
+    });
+  } catch (e) {
+    return res.status(400).json({ error: e.message || "something went wrong" });
+  }
+};
+
+module.exports = {
+  addTaskController,
+  editTaskController,
+  deleteTaskController,
+  allTaskController,
+  viewSingleTaskController,
+};
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -14,9 +14,17 @@ router.use(verifyToken)
 router.route("/add_todo").post(taskController.addTaskController);
 
 // edit task
-router.route("/edit_todo").post(taskController.editTaskController);
+router.route("/edit_todo/:todoId").patch(taskController.editTaskController);
 
 // delete task
-router.route("/delete_todo").delete(taskController.deleteTaskController);
+router.route("/delete_todo/:todoId").delete(taskController.deleteTaskController);
+
+// get all todo
+router.route("/all_todo").get(taskController.allTaskController);
+
+// view single todo
+router.route("/view_todo/:todoId").get(taskController.viewSingleTaskController);
+
+
 
 module.exports = router;
